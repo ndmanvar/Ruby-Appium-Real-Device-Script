bcbbc9121ef13c71b13d79feac338da1603b6f76
Neil <neil@Neils-MacBook-Pro.local>
2015-09-25 17:07:19 -0700

    Adding simple ruby iphone 6 real device example

---
+++ sample_real_device_test.rb
@@ -0,0 +1,19 @@
+require "appium_lib"
+
+caps = {
+  caps: {
+    deviceName: 'iPhone 6 Device',
+    deviceOrientation: 'portrait',
+    platformVersion: "8.0",
+    platformName: "iOS",
+    app: 'sauce-storage:TestApp-iphoneos.app.zip',
+    name: 'sample iPhone 6 real device test'
+  }
+}
+
+@driver = Appium::Driver.new(caps)
+@driver.start_driver
+
+@driver.find_elements(:accessibility_id, "TextField1").send_keys "12"
+
+@driver.driver_quit
